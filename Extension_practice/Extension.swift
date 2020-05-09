82e91cbe7f6c27038d981f4676de1c27bdddef29
Jeff, Jeong  정 의정 <35421421+TuenTuenna@users.noreply.github.com>
2020-05-10 02:03:49 +0900

    extension added

---
+++ Extension_practice/Extension.swift
@@ -0,0 +1,18 @@
+//
+//  Extension.swift
+//  Extension_practice
+//
+//  Created by Jeff Jeong on 2020/05/10.
+//  Copyright © 2020 Tuentuenna. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+    
+    func sayHi() {
+        print("하이!!")
+    }
+    
+}
---
+++ Extension_practice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Extension_practice
+//
+//  Created by Jeff Jeong on 2020/05/10.
+//  Copyright © 2020 Tuentuenna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        sayHi()
+        
+    }
+
+
+    
+    
+}
+
